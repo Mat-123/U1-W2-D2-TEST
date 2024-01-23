6060b2b1047bb6bfd6bbcb51a8d4206c9334f671
Mat-123 <156803363+Mat-123@users.noreply.github.com>
2024-01-23 16:25:49 +0100

    -update

--- D2.js
+++ D2.js
@@ -2,28 +2,63 @@
  Scrivi un algoritmo per trovare il più grande tra due numeri interi.
 */
 
-let num1 = 10
-let num2 = 15
+/* RISPOSTA 1
+const num1 = 10;
+const num2 = 15;
 
-if 
+if (num1 > num2) {
+  alert("Il " + num1 + " è maggiore di " + num2);
+  console.log("Il " + num1 + " è maggiore di " + num2);
+} else {
+  alert("Il " + num2 + " è maggiore di " + num1);
+  console.log("Il " + num2 + " è maggiore di " + num1);
+}
++/
 
 /* ESERCIZIO 2
   Scrivi un algoritmo che mostri "not equal" in console se un numero intero fornito è diverso da 5.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* RISPOSTA 2
+const num1 = 7;
+
+if (num1 === 5) {
+  console.log("Il numero indicato è uguale a 5.");
+} else {
+  console.log("Il numero indicato è diverso da 5.");
+}
+*/
 
 /* ESERCIZIO 3
   Scrivi un algoritmo che mostri "divisibile per 5" in console se un numero fornito è perfettamente divisibile per 5 (suggerimento: usa l'operatore modulo)
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* RISPOSTA 3
+const num1 = 20;
+
+if (num1 % 5 == 0) {
+  console.log("Il numero indicato è divisibile per 5.");
+} else {
+  console.log("Purtroppo il numero indicato non è divisibile per 5.");
+}
+*/
 
 /* ESERCIZIO 4
   Scrivi un algoritmo per verificare che, dati due numeri interi, il valore di uno di essi sia 8 oppure se la loro addizione/sottrazione sia uguale a 8.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/*
+const num1 = 12;
+const num2 = 20;
+
+if (num1 === 8 || num2 === 8) {
+  console.log("Uno dei due numeri è uguale a 8");
+} else if (num1 + num2 === 8) {
+  console.log("La somma tra " + num1 + " e " + num2 + "è 8.");
+} else {
+  console.log("La differenza tra " + num2 + " e " + num1 + " è 8.");
+}
+*/
 
 /* ESERCIZIO 5
   Stai lavorando su un sito di e-commerce. Stai salvando il saldo totale del carrello dell'utente in una variabile "totalShoppingCart".
@@ -31,14 +66,72 @@ if
   Crea un algoritmo che determini l'ammontare totale che deve essere addebitato all'utente per il checkout.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* RISPOSTA 5
+const product1 = 20;
+const product2 = 15;
+const product3 = 14;
+const product4 = 3;
+
+const shipfees = 10;
+
+let productsubtotal = product1 + product2 + product3 + product4;
+
+console.log("Totale provvisorio del carrello: " + productsubtotal);
+
+if (productsubtotal > 50) {
+  console.log(
+    `Il suo carrello aderisce all'offerta "Spedizione gratuita" pertanto il totale è ` + productsubtotal + "€."
+  );
+} else {
+  console.log(
+    "Purtroppo il suo totale provvisorio è " +
+      productsubtotal +
+      "€" +
+      ", inferiore al minimo previsto per aderire alla spedizione gratuita, la invitiamo ad aggiungere ulteriori articoli oppure a completare l'ordine per un totale compreso di spedizioni di " +
+      (productsubtotal + shipfees) +
+      "€."
+  );
+}
+
+*/
 
 /* ESERCIZIO 6
   Stai lavorando su un sito di e-commerce. Oggi è il Black Friday e viene applicato il 20% su ogni prodotto.
   Modifica la risposta precedente includendo questa nuova promozione nell'algoritmo, determinando come prima se le spedizioni sono gratuite oppure no e e calcolando il totale.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* RISPOSTA 6
+const product1 = 20;
+const product2 = 15;
+const product3 = 14;
+const product4 = 3;
+const bfdiscount = 20;
+
+const shipfees = 10;
+
+let productsubtotal = product1 + product2 + product3 + product4;
+
+let discountedsubt = productsubtotal - (productsubtotal * bfdiscount) / 100;
+
+console.log("Totale provvisorio del carrello: " + discountedsubt);
+
+console.log;
+
+if (discountedsubt > 50) {
+  console.log(
+    `Il suo carrello aderisce all'offerta "Spedizione gratuita" pertanto il totale è ` + discountedsubt + "€."
+  );
+} else {
+  console.log(
+    "Purtroppo il suo totale provvisorio è " +
+      discountedsubt +
+      "€" +
+      ", inferiore al minimo previsto per aderire alla spedizione gratuita, la invitiamo ad aggiungere ulteriori articoli oppure a completare l'ordine per un totale compreso di spedizioni di " +
+      (discountedsubt + shipfees) +
+      "€."
+  );
+}
+*/
 
 /* ESERCIZIO 7
   Crea tre variabili, e assegna un valore numerico a ciascuna di esse.
@@ -46,7 +139,11 @@ if
   Alla fine mostra il risultato in console.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* RISPOSTA 7
+const numbers = [63, 26, 44, 1];
+numbers.sort();
+console.log(numbers);
+*/
 
 /* ESERCIZIO 8
   Crea un algoritmo per verificare che un valore fornito sia un numero oppure no (suggerimento: cerca su un motore di ricerca "typeof").
@@ -78,11 +175,11 @@ if
   Fornito il seguente oggetto, scrivi del codice per aggiungere una proprietà "city", il cui valore sarà "Toronto".
 */
 
-const me = {
-  name: 'John',
-  lastName: 'Doe',
-  skills: ['javascript', 'html', 'css'],
-}
+/*const me = {
+  name: "John",
+  lastName: "Doe",
+  skills: ["javascript", "html", "css"],
+};*/
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
